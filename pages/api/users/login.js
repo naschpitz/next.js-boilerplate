9d422fe1081ca6172b2b20fcd72af5d4c3a7aa24
Bruno Naschpitz <naschpitz@gmail.com>
2020-11-23 08:22:16 -0300

    Improved error handling for "/api/users/login".

--- pages/api/users/login.js
+++ pages/api/users/login.js
@@ -5,17 +5,20 @@ export default async function login(req, res) {
   if (req.method === "POST") {
     const { email, password } = req.body;
 
-    const user = await Users.findOne().byEmailPassword(email, password);
+    if (!email || !password)
+      return res.status(400).json({ message: "Missing fields to login." });
+
+    const user = await Users.findOne().byEmailPassword(email, password).select("email.address email.verified role");
 
     if (!user)
-      return res.status(403).json({ message: "Wrong username and/or password." });
+      return res.status(404).json({ message: "Wrong username and/or password." });
 
     const userId = user._id;
 
     const session = await Sessions.findOne().byOwner(userId);
     await session.genToken(req, res);
 
-    return res.status(201).json({});
+    return res.status(201).json(user);
   }
 
   return res.status(405).json({ message: "Method not allowed." });
