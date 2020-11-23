8751726ab78036957d7ab8da7b86793cbcba6397
Bruno Naschpitz <naschpitz@gmail.com>
2020-11-23 08:21:43 -0300

    Improved error handling for "/api/users/info".

--- pages/api/users/info.js
+++ pages/api/users/info.js
@@ -6,17 +6,17 @@ export default async function info(req, res) {
     const isValid = await Sessions.isValid(req, res);
 
     if (!isValid)
-      return res.status(403).json({ message: "Invalid session token." });
+      return res.status(401).json({ message: "Invalid session token." });
 
     const sessionCookieObj = Sessions.getCookieObject(req, res);
 
     const userId = sessionCookieObj.userId;
-    const user = await Users.findById(userId).select('email.address email.verified');
+    const user = await Users.findById(userId).select('email.address email.verified role');
 
     const session = await Sessions.findOne().byOwner(userId);
     await session.genToken(req, res);
 
-    return res.status(200).json({ user });
+    return res.status(200).json(user);
   }
 
   return res.status(405).json({ message: "Method not allowed." });
